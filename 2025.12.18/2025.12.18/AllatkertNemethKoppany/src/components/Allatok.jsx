7bfe945c04169c9b4464dc5b32a3073925244c5c
nemethkoppany <koppany344@gmail.com>
2026-01-07 23:20:57 +0100

    idk

--- 2025.12.18/2025.12.18/AllatkertNemethKoppany/src/components/Allatok.jsx
+++ 2025.12.18/2025.12.18/AllatkertNemethKoppany/src/components/Allatok.jsx
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react";
 
 export default function Allatok() {
-  
-    const [allatok, setAllatok] = useState([]);
+  const [allatok, setAllatok] = useState([]);
 
-    useEffect(()=>{
-        async function getAllatok() {
-            const response = await fetch("http://localhost:5000/allatok");
-            const resData = await response.json();
-            const rendezett = resData.sort((a,b) => a.gondozo > b.gondozo? 1:-1);
-            setAllatok(rendezett);
-        }
-        getAllatok();
-    },[]);
+  useEffect(() => {
+    async function getAllatok() {
+      const response = await fetch("http://localhost:5000/allataink");
+      const resData = await response.json();
+      const rendezett = resData.sort((a, b) =>
+        a.gondozo.localeCompare(b.gondozo)
+      );
+      setAllatok(rendezett);
+    }
 
+    getAllatok();
+  }, []);
 
-    return (
+  return (
     <>
       <header className="text-center m-5">
-        <h1>Állatkertünk jelenlegi lakó</h1>
+        <h1>Állatkertünk jelenlegi lakói</h1>
       </header>
       <main className="m-5">
-        <table className="table table-striped">
-          <thead>
+        <table className="table table-striped table-bordered">
+          <thead className="table-dark">
             <tr>
               <th>Állat neve</th>
               <th>Faj</th>
@@ -32,20 +33,16 @@ export default function Allatok() {
             </tr>
           </thead>
           <tbody>
-            {
-                allatok.length > 0 && 
-                    allatok.map((allat) => (
-                    <tr key={allat.nev}>
-                        <td>{allat.nev}</td>
-                        <td>{allat.faj}</td>
-                        <td>{allat.gondozo}</td>
-                        <td>{allat.helye}</td>
-                        <td>{allat.erkezes}</td>
-                        
-                    </tr>
-                    )
-                )
-            }
+            {allatok.length > 0 &&
+              allatok.map((allat, index) => (
+                <tr key={index}>
+                  <td>{allat.nev}</td>
+                  <td>{allat.faj}</td>
+                  <td>{allat.gondozo}</td>
+                  <td>{allat.helye}</td>
+                  <td>{allat.erkezes}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </main>
